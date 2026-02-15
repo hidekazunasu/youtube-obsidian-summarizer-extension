4301c7a40c16858bccdb1abed4fb52cfda167116
hidekazu nasu <n-solutionshien@nscd.nstac.go.jp>
2026-02-15 22:07:42 +0900

    bug fix

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -3,7 +3,7 @@ import { sanitizePathSegment } from '../src/lib/utils';
 
 describe('sanitizePathSegment', () => {
   it('removes path-invalid characters', () => {
-    expect(sanitizePathSegment('A:/B*?"<>|C')).toBe('A--B-----C');
+    expect(sanitizePathSegment('A:/B*?"<>|C')).toBe('A--B------C');
   });
 
   it('falls back to untitled for blank strings', () => {
